f2beb748c1ac667fbe51a2337c6917124a9f62d9
z1z1z1x <157633853+z1z1z1x@users.noreply.github.com>
2024-01-24 13:15:18 -0500

    Initial Commit
    test

---
+++ script.js
@@ -0,0 +1,13 @@
+var photo = $('.item');
+
+function changeToGroupPhoto() {
+  photo.attr('src', 'assets/Themepark_GroupPhoto.jpg');
+}
+
+function changeToJob1() {
+  photo.attr('src', 'assets/Themepark_Job1.png');
+}
+
+function changeToJob2() {
+  photo.attr('src', 'assets/Themepark_Job2.jpg');
+}
